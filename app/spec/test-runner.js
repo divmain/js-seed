51cf7452bffaacda45a0058961c6f7e59f52c29a
Dale Bustad <dale.bustad@formidablelabs.com>
2014-03-30 23:53:29 -0700

    make sinon work

--- app/spec/test-runner.js
+++ app/spec/test-runner.js
@@ -2,22 +2,10 @@ document.write("<div id=\"mocha\"></div>");
 
 require("!style!css!mocha/mocha.css");
 require("!script!mocha/mocha.js");
+require("!script!sinon/pkg/sinon.js");
 
 window.expect = require("chai").expect;
 
-window.sinon = _.extend({}, require("sinon"), {
-  spy: require("sinon/lib/sinon/spy"),
-  spyCall: require("sinon/lib/sinon/call"),
-  behavior: require("sinon/lib/sinon/behavior"),
-  stub: require("sinon/lib/sinon/stub"),
-  // mock: require("sinon/lib/sinon/mock"),
-  collection: require("sinon/lib/sinon/collection"),
-  assert: require("sinon/lib/sinon/assert"),
-  sandbox: require("sinon/lib/sinon/sandbox"),
-  test: require("sinon/lib/sinon/test"),
-  testCase: require("sinon/lib/sinon/test_case"),
-  match: require("sinon/lib/sinon/match")
-});
 
 mocha.setup("bdd");
 
