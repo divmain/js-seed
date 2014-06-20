d12524fc32a50be07dec3f4370b3c01670fe685e
Dale Bustad <dale.bustad@formidablelabs.com>
2014-06-20 16:13:02 -0700

    update start script

--- start.sh
+++ start.sh
@@ -1,29 +1,46 @@
 CURRENTDIR="`pwd`"
-DEFAULT_FULLNAME="`git config user.name`"
-DEFAULT_EMAIL="`git config user.email`"
 
-read -p "Enter name: ($DEFAULT_FULLNAME)" FULLNAME
-read -p "Email: ($DEFAULT_EMAIL)" EMAIL
+###############
 
-read -r -p  -n 1 "Would you like to create a GitHub repo (y/n)? " DO_GITHUB
+read -r -p "would you like to create a new github repo (y/n)? " -n 1 DO_GITHUB
 echo
-if [[ $DO_GITHUB =~ ^[Yy]$ ]]
-then
-    read -p "Name of Github repo: " GITHUB_REPO
-    read -p "GitHub Username: " GITHUB_USER
-    read -p "GitHub Password: " GITHUB_PWD
+if [[ $DO_GITHUB =~ ^[Yy]$ ]]; then
+    read -p "name of repo: " GITHUB_REPO
+    read -p "github username: " GITHUB_USER
+    read -s -p "github password: " GITHUB_PWD
 fi
+
+###############
+
+echo -n "cleaning up... "
 
 rm -f $CURRENTDIR/start.sh
 rm -rf $CURRENTDIR/.git
+echo "OK"
 
-git init
-git add .
-git commit -m "initial commit"
+###############
 
-if [[ $DO_GITHUB =~ ^[Yy]$ ]]
-then
-    curl -u "$GITHUB_USER:$GITHUB_PWD" https://api.github.com/user/repos -d '{"name":"$GITHUB_REPO"}'
-    git remote add origin git@github.com:$GITHUB_USER/GITHUB_REPO.git
-    git push origin master
+echo -n "initializing git... "
+
+git init > /dev/null
+git add . > /dev/null
+git commit -m "initial commit" > /dev/null
+
+echo "OK"
+
+###############
+
+if [[ $DO_GITHUB =~ ^[Yy]$ ]]; then
+    echo -n "creating github repository... "
+    GITHUB_RESPONSE=$(curl -s -o /dev/null -w "%{http_code}" -u "$GITHUB_USER:$GITHUB_PWD" https://api.github.com/user/repos -d '{"name":"'"$GITHUB_REPO"'"}')
+    if [ "$GITHUB_RESPONSE"="201" ]; then
+        echo "OK"
+        echo -n "pushing to github... "
+        git remote add origin git@github.com:$GITHUB_USER/$GITHUB_REPO.git > /dev/null
+        git push origin master -q
+        echo "OK"
+    else
+        echo "FAILURE"
+        echo "  http response code $GITHUB_RESPONSE"
+    fi
 fi
