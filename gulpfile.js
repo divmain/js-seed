efc821997d27dc814b0122f7fd5588d58eb9a381
Dale Bustad <dale.bustad@formidablelabs.com>
2014-10-01 15:42:35 -0700

    fail and return code 1 when lint fails

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var _frontendTest,
   path = require("path"),
   _ = require("lodash"),
+  map = require("map-stream"),
 
   // Gulp
   gulp = require("gulp"),
@@ -70,13 +71,26 @@ gulp.task("copy", false, function () {
 });
 
 gulp.task("lint", "Lint application- and test-code.", function () {
+  var success = true;
+
   return gulp.src([
     path.join(config.srcFullPath, "**/*.js"),
     path.join(config.root, config.spec, "**/*.js"),
     path.join(config.root, "*.js")
   ])
     .pipe(eslint())
-    .pipe(eslint.format());
+    .pipe(map(function (file, output) {
+      success = success && _.every(file.eslint && file.eslint.messages, function (message) {
+        return message.severity !== 2;
+      });
+      return output(null, file);
+    }))
+    .pipe(eslint.format())
+    .on("end", function () {
+      if (!success) {
+        throw new Error("*** FAILED ESLINT ***");
+      }
+    });
 });
 
 gulp.task("reload:server", false, function () {
