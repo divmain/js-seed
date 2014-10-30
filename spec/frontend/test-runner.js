d35912de6453be8832aff2fa5e82c6632c1ba57a
Dale Bustad <dale@divmain.com>
2014-10-29 23:25:32 -0700

    remove old cover-js logic from test runner

--- spec/frontend/test-runner.js
+++ spec/frontend/test-runner.js
@@ -4,12 +4,3 @@ window.sinon = require("sinon");
 window.chai.use(require("sinon-chai"));
 
 require("spec/frontend/test-entry");
-
-after(function() {
-  var coverJsEl, mochaEl;
-  require("coverjs-loader").reportHtml();
-
-  coverJsEl = document.querySelector(".coverjs-report");
-  mochaEl = document.querySelector("#mocha");
-  if (coverJsEl) { mochaEl.appendChild(coverJsEl); }
-});
