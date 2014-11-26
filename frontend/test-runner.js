6b4579f3634c5b2c324115150310c8de351b9940
Dale Bustad <dale@divmain.com>
2014-11-26 01:47:25 -0800

    document test-runner.js

--- frontend/test-runner.js
+++ frontend/test-runner.js
@@ -1,7 +1,24 @@
 var _ = require("lodash");
-// https://github.com/webpack/docs/wiki/context/
-//   71f80252750e1d8f4b6b40395855e0dd5cca6d12#requirecontext
+
+/**
+ * This is the Webpack voo-doo that makes it so easy to add spec
+ * files.  It will recursively traverse the current directory,
+ * checking for any files that end in .spec.js, bundling them
+ * and making them accessible via the special `specFileRequire`
+ * function.  These test modules are explicitly required in below.
+ *
+ * For more information, read the documentation here:
+ *   https://github.com/webpack/docs/wiki/context/
+ *   71f80252750e1d8f4b6b40395855e0dd5cca6d12#requirecontext
+ */
+
 var specFileRequire = require.context(".", true, /\.spec\.js$/);
+
+/**
+ * This creates a DOM element with ID "fixtures".  It is visible,
+ * but far off to the left of the viewable screen.  When testing
+ * your views, they can be mounted here.
+ */
 
 var fixturesEl = document.createElement("div");
 fixturesEl.setAttribute("id", "fixtures");
@@ -9,16 +26,21 @@ document.querySelector("body").appendChild(fixturesEl);
 fixturesEl.style.position = "absolute";
 fixturesEl.style.left = "5000px";
 
-// polyfill Object.keys for IE<=8
-if (!Object.keys) {
-  Object.keys = _.keys;
-}
+/**
+ * This loads Chai (assertion library) and Sinon (mock/stub
+ * library) into the global context, for both in-browser
+ * and Karma tests.
+ */
 
 window.chai = require("chai");
 window.expect = window.chai.expect;
 window.sinon = require("sinon");
 window.chai.use(require("sinon-chai"));
 
-_.each(specFileRequire.keys(), function (specFileName) {
+/**
+ * Requires in all spec files.  See above comment.
+ */
+
+_.each(_.keys(specFileRequire), function (specFileName) {
   specFileRequire(specFileName);
 });
