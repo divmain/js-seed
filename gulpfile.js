f88474ccf4dcd9ee5ff965b5410db2461e64bd94
Dale Bustad <dale@divmain.com>
2014-11-26 01:25:28 -0800

    document gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,22 @@
-/*
-  Rather than manage one giant gulpfile, each task has been broken out into its own file in the
-  tasks directory. Any files in that directory get automatically required by the line below.
-  To add a new task, simply add a new task file that directory.
-*/
-
 var
   gulp = require("gulp"),
   gulpHelp = require("gulp-help"),
   requireDir = require("require-dir");
 
-// Update `gulp.task` signature to take task descriptions.
+/**
+ * This modifies the `gulp.task` function to take an additional argument.
+ * The argument should be a short string that describes the task; it will
+ * be displayed when `gulp help` is run from the console.
+ */
+
 gulpHelp(gulp);
+
+/**
+ * Rather than manage one giant gulpfile, each task is broken out into its
+ * own file in the `gulp-tasks` directory.  Any files in that directory will be
+ * automatically required by the line below.
+ *
+ * To add a new task, simple add a new file under `gulp-tasks`.
+ */
 
 requireDir("./gulp-tasks", { recurse: true });
